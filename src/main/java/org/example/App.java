83c4957b66eec0e80d470d605853ae85aabdcbeb
Gilberto Falco <gilberto.falco@outlook.com>
2020-03-29 17:27:24 -0300

    Alteracoes semana 2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,19 +5,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.HashSet;
+import java.util.TreeSet;
 import java.util.Iterator;
-
 
 public class App {
     
     private static Scanner sc = new Scanner(System.in);
-    private static Set<Aluno> listaAlunos = new HashSet<Aluno>();
+    private static TreeSet<String> listaAlunos = new TreeSet<String>();
    
     public static void main(String[] args) {
         int i = 0;
         String option;
-    
+        
         do {
 
             System.out.println("\n----- Menu -----");
@@ -44,29 +43,26 @@ public class App {
         System.out.println("Insira o nome: ");
         String nome = sc.nextLine();
         if(verificaNomeExistente(nome)){
-            listaAlunos.add(new Aluno(nome));
+            listaAlunos.add(nome);
         }else{
             System.out.println("\n NOME JA CADASTRADO! \n");
-        }
-        
+        } 
    }
     
     private static boolean verificaNomeExistente(String nome){
-        Iterator<Aluno> i = listaAlunos.iterator();
+        Iterator<String> i = listaAlunos.iterator();
         while (i.hasNext()){
-            Aluno nomes = i.next();
-            if(nomes.getNome().equals(nome)){
+            String nomes = i.next();
+            if(nomes.equals(nome)){
                 return false;
             }   
         }
         return true;
-   
     }
-    
     
     private static void exibirLista() {
         System.out.println("----- Lista de nomes -----");
-        Iterator<Aluno> i = listaAlunos.iterator();
+        Iterator<String> i = listaAlunos.iterator();
         while (i.hasNext()){
             System.out.println(i.next()); 
         }
